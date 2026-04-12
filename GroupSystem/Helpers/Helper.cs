278da4f536d9f80e4ef416c01163dc12c8731277
aysuvm <aysuvm-apa103@code.edu.az>
2026-04-12 20:39:19 +0400

    task

---
+++ GroupSystem/Helpers/Helper.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace GroupSystem.Helpers
+{
+    public class Helper
+    {
+        public static void PrintConsole(ConsoleColor color, string text)
+        {
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+        }
+    }   
+}
