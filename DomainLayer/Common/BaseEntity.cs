7801c52a105ff74c189678e31d0eedc58b0640bd
aysuvm <aysuvm-apa103@code.edu.az>
2026-04-12 20:36:38 +0400

    task was

---
+++ DomainLayer/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace DomainLayer.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ DomainLayer/Entities/CourseGroup.cs
@@ -0,0 +1,22 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entities
+{
+    public class CourseGroup:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public string Room { get; set; }
+        public object SeatCount { get; set; }
+
+        public static implicit operator string(CourseGroup v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
